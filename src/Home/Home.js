5990aa5701c4291b1744fd1f2c9ac3af49156412
Dylan Floyd <floyd800@gmail.com>
2021-09-27 17:07:04 -0700

    add very lazy home page styling

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                welcome home bud
+            <div className="flexCol">
+                <h1>PokeDex</h1>
+                <p>Woooo pokemon!!1!</p>
+                <p>Sorry for phoning this one in Erich, I'm lacking enthusiam for home page copy today.</p>
             </div>
         )
     }
